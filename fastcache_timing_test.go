b2c42a042e9447b2fccb7fd44b33c508e2779f90
Jon Williams <jonathan.williams@gmail.com>
2018-11-26 10:02:14 -0500

    Added sync.Map to benchmarks

--- fastcache_timing_test.go
+++ fastcache_timing_test.go
@@ -299,3 +299,85 @@ func BenchmarkStdMapSetGet(b *testing.B) {
 		}
 	})
 }
+
+func BenchmarkSyncMapSet(b *testing.B) {
+	const items = 1 << 16
+	m := sync.Map{}
+	b.ReportAllocs()
+	b.SetBytes(items)
+	b.RunParallel(func(pb *testing.PB) {
+		k := []byte("\x00\x00\x00\x00")
+		v := "xyza"
+		for pb.Next() {
+			for i := 0; i < items; i++ {
+				k[0]++
+				if k[0] == 0 {
+					k[1]++
+				}
+				m.Store(string(k), v)
+			}
+		}
+	})
+}
+
+func BenchmarkSyncMapGet(b *testing.B) {
+	const items = 1 << 16
+	m := sync.Map{}
+	k := []byte("\x00\x00\x00\x00")
+	v := "xyza"
+	for i := 0; i < items; i++ {
+		k[0]++
+		if k[0] == 0 {
+			k[1]++
+		}
+		m.Store(string(k), v)
+	}
+
+	b.ReportAllocs()
+	b.SetBytes(items)
+	b.RunParallel(func(pb *testing.PB) {
+		k := []byte("\x00\x00\x00\x00")
+		for pb.Next() {
+			for i := 0; i < items; i++ {
+				k[0]++
+				if k[0] == 0 {
+					k[1]++
+				}
+				vv, ok := m.Load(string(k))
+				if !ok || vv.(string) != string(v) {
+					panic(fmt.Errorf("BUG: unexpected value; got %q; want %q", vv, v))
+				}
+			}
+		}
+	})
+}
+
+func BenchmarkSyncMapSetGet(b *testing.B) {
+	const items = 1 << 16
+	m := sync.Map{}
+	b.ReportAllocs()
+	b.SetBytes(2 * items)
+	b.RunParallel(func(pb *testing.PB) {
+		k := []byte("\x00\x00\x00\x00")
+		v := "xyza"
+		for pb.Next() {
+			for i := 0; i < items; i++ {
+				k[0]++
+				if k[0] == 0 {
+					k[1]++
+				}
+				m.Store(string(k), v)
+			}
+			for i := 0; i < items; i++ {
+				k[0]++
+				if k[0] == 0 {
+					k[1]++
+				}
+				vv, ok := m.Load(string(k))
+				if !ok || vv.(string) != string(v) {
+					panic(fmt.Errorf("BUG: unexpected value; got %q; want %q", vv, v))
+				}
+			}
+		}
+	})
+}
