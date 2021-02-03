ce8a2bb6fae55e2b1aa7cf31f20945fecbb3542b
Richard Artoul <richardartoul@gmail.com>
2021-02-02 18:49:50 -0600

    Fix surprising / unexpected behavior with `GetBig` when testing for cache misses

--- bigcache.go
+++ bigcache.go
@@ -75,7 +75,15 @@ func (c *Cache) SetBig(k, v []byte) {
 func (c *Cache) GetBig(dst, k []byte) []byte {
 	atomic.AddUint64(&c.bigStats.GetBigCalls, 1)
 	subkey := getSubkeyBuf()
-	defer putSubkeyBuf(subkey)
+	dstWasNil := dst == nil
+	defer func() {
+                putSubkeyBuf(subkey)
+                if len(r) == 0 && dstWasNil {
+                        // Guarantee that if the caller provided nil and this is a cache miss that
+                        // the caller can accurately test for a cache miss with `if r == nil`.
+                        r = nil
+                }
+        }()
 
 	// Read and parse metavalue
 	subkey.B = c.Get(subkey.B[:0], k)
