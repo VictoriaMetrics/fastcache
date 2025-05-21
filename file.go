4905d51e15bf49f235d18f248a7a27d8a5d1da8f
Maksim Kotlyar <kotlyar.maksim@gmail.com>
2025-05-21 16:19:11 +0300

    file: load from file should allow configure maxBytes
    It is needed to implement same behavior as load from file or new does
    (specifically this check
    https://github.com/VictoriaMetrics/fastcache/blob/e439f07b570777a09fb9b662e47bd52d895c349b/file.go#L133-L139)
    
    Related to
    https://github.com/VictoriaMetrics/VictoriaMetrics/pull/8952/files

--- file.go
+++ file.go
@@ -79,8 +79,8 @@ func (c *Cache) SaveToFileConcurrent(filePath string, concurrency int) error {
 // LoadFromFile loads cache data from the given filePath.
 //
 // See SaveToFile* for saving cache data to file.
-func LoadFromFile(filePath string) (*Cache, error) {
-	return load(filePath, 0)
+func LoadFromFile(filePath string, maxBytes int) (*Cache, error) {
+	return load(filePath, maxBytes)
 }
 
 // LoadFromFileOrNew tries loading cache data from the given filePath.
